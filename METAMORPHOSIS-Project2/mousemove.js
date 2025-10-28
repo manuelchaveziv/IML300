f06cefe127f0b01c5cb3bfa0faa48f151f2def90
Manuel <juanmcha@usc.edu>
2025-10-28 16:52:30 -0700

    Changes

--- METAMORPHOSIS-Project2/mousemove.js
+++ METAMORPHOSIS-Project2/mousemove.js
@@ -4,20 +4,16 @@ function setup() {
 }
 
 function draw() {
-  // Subtle fade effect for soft trailing
   background(0, 0, 0, 30);
   
-  // Draw many layers with very low opacity for soft glow
   noStroke();
   
-  // Create soft gradient with many layers
   for (let r = 120; r > 0; r -= 8) {
-    let alpha = map(r, 0, 120, 8, 0); // Very low opacity values
+    let alpha = map(r, 0, 120, 8, 0); 
     fill(0, 255, 0, alpha);
     circle(mouseX, mouseY, r);
   }
   
-  // Soft core
   fill(0, 255, 150, 15);
   circle(mouseX, mouseY, 20);
 }
